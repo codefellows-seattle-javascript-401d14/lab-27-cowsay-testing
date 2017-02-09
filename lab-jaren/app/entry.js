2a17ae85427c8e55d6ce7e7fe8f779605dc9d002
jjron <jaren.escueta@gmail.com>
2017-02-08 19:23:27 -0800

    added viewCow undoCow and tests

--- lab-jaren/app/entry.js
+++ lab-jaren/app/entry.js
@@ -11,12 +11,24 @@ cowApp.component('cowsay', {
   template: require('./cowsay.html'),
   controller: [function() {
     this.$onInit = function() {
-      this.title = 'cows are alright';
+      this.title = 'cow creator.';
+      this.tagline = 'make it, view it, and undo it!';
       this.userInput = '';
       this.history = [];
+      this.currentCow = '';
 
       this.getCow = function(text) {
-        return cowsayBrowser.say({text: text || 'MOOOYAH!!', f: 'supermilker'});
+        return cowsayBrowser.say({text: text || 'cowcontrol.io'});
+      };
+
+      this.viewCow = function() {
+        this.history.push(this.getCow(this.userInput));
+        this.currentCow = this.history[this.history.length - 1];
+      };
+
+      this.undoCow = function() {
+        this.history.pop();
+        this.currentCow = this.history[this.history.length - 1];
       };
     };
   }],
--- lab-jaren/test/cowsay-test.js
+++ lab-jaren/test/cowsay-test.js
@@ -21,7 +21,8 @@ describe('testing cowsay component\'s controller', function() {
 
   describe('testing initial state', () => {
     it('should initialize methods and properties', done => {
-      expect(this.cowsayCtrl.title).to.equal('cows are alright');
+      expect(this.cowsayCtrl.title).to.equal('cow creator.');
+      expect(this.cowsayCtrl.tagline).to.equal('make it, view it, and undo it!');
       expect(this.cowsayCtrl.userInput).to.equal('');
       expect(this.cowsayCtrl.history).to.be.instanceof(Array);
       expect(this.cowsayCtrl.history.length).to.equal(0);
@@ -31,16 +32,38 @@ describe('testing cowsay component\'s controller', function() {
   });
 
   describe('testing getCow', () => {
-    it('should return a cow that says "MOOOYAH!!"', done => {
+    it('should return a cow that says "cowcontrol.io"', done => {
       let result = this.cowsayCtrl.getCow();
-      expect(result).to.equal(cowsayBrowser.say({text: 'MOOOYAH!!', f: 'supermilker'}));
+      expect(result).to.equal(cowsayBrowser.say({text: 'cowcontrol.io'}));
       done();
     });
 
     it('should return a cow that says hello', done => {
       this.cowsayCtrl.userInput = 'hello';
       let result = this.cowsayCtrl.getCow(this.cowsayCtrl.userInput);
-      expect(result).to.equal(cowsayBrowser.say({text: 'hello', f: 'supermilker'}));
+      expect(result).to.equal(cowsayBrowser.say({text: 'hello'}));
+      done();
+    });
+  });
+
+  describe('testing viewCow', () => {
+    it('should return a cow that says "cowabunga!"', done => {
+      this.cowsayCtrl.userInput = 'cowabunga!';
+      this.cowsayCtrl.viewCow();
+      expect(this.cowsayCtrl.currentCow).to.equal(cowsayBrowser.say({text: 'cowabunga!'}));
+      done();
+    });
+  });
+
+  describe('testing undoCow', () => {
+    it('should return ', done => {
+      this.cowsayCtrl.userInput = 'mooyah';
+      this.cowsayCtrl.viewCow();
+      this.cowsayCtrl.userInput = 'grass is life';
+      this.cowsayCtrl.viewCow();
+      this.cowsayCtrl.undoCow();
+      expect(this.cowsayCtrl.currentCow).to.equal(cowsayBrowser.say({text: 'mooyah'}));
+      expect(this.cowsayCtrl.history.length).to.equal(1);
       done();
     });
   });
